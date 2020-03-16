58d69ef47385005e52ded8a0d286a05217c33580
LusioF <lusiof@gmail.com>
2020-03-16 13:51:56 -0400

    return function

--- models/index.js
+++ models/index.js
@@ -13,3 +13,13 @@ if (config.use_env_variable) {
   } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
   }
+
+  fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+  })
+  .forEach(function(file) {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
