721cb5ebce7132025a881e8d438f0c291379c00e
LusioF <lusiof@gmail.com>
2020-03-16 13:47:16 -0400

    config js file

---
+++ config/config.js
@@ -0,0 +1,26 @@
+// Include the dotenv npm package
+require('dotenv').config();
+module.exports =
+{
+
+  "development": {
+    "username": "root",
+    "password": "testtest",
+    "database": "burgers_db",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable":"JAWSDB_URL",
+    
+    "dialect": "mysql"
+  }
+}
