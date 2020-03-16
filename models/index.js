98f094d925dcc37e77b5f3e0610ebe6e39f78978
LusioF <lusiof@gmail.com>
2020-03-16 13:52:57 -0400

    sequelize and module exports

--- models/index.js
+++ models/index.js
@@ -23,3 +23,14 @@ if (config.use_env_variable) {
     var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
+
+  Object.keys(db).forEach(function(modelName) {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+  
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+  
+  module.exports = db;
